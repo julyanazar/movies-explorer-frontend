b43eb4ef853ed07e440429e5027748a5ab364ba1
julyanazar <nnwrnuwr@gmail,com>
2021-09-07 20:09:24 +0400

    feat: реализовать компонент SearchForm форма поиска, куда пользователь будет вводить запрос

--- src/components/FilterCheckBox/FilterCheckBox.js
+++ src/components/FilterCheckBox/FilterCheckBox.js
@@ -1,6 +1,8 @@
+import React from 'react';
+import { useState } from "react";
+
 import "./FilterCheckBox.css";
 import Switch from "../Switch/Switch";
-import { useState } from "react";
 
 const FilterCheckBox = () => {
 
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,18 +1,10 @@
 import React from 'react';
-import Promo from '../Promo/Promo';
-import AboutProject from '../AboutProject/AboutProject';
-import Techs from '../Techs/Techs';
-import AboutMe from '../AboutMe/AboutMe';
-import Portfolio from '../Portfolio/Portfolio';
+import SearchForm from '../SearchForm/SearchForm';
 
 const Main = () => {
     return (
         <main className="main">
-            <Promo />
-            <AboutProject />
-            <Techs />
-            <AboutMe />
-            <Portfolio />
+            <SearchForm />
         </main>
     );
 };
